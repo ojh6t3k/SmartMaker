c677043114a6f9afdb9b72d971cc6fc00322f003
Jaehong, Oh <jhoh@idealink.kr>
2015-04-02 21:43:00 +0900

    v1.0.0b1
    배포 준비 완료

---
+++ Assets/SmartMaker/ReadMe.txt
@@ -0,0 +1,22 @@
+Smart Maker for Unity3D
+
+[Release]
+-Version: v1.0.0b1
+-Date: April/1/2015
+
+
+[What's purpose]
+Smart Maker is designed to produce a very good App content using the Unity3D and Arduino.
+
+
+[How to use]
+Coming soon...
+
+
+[License]
+Copyright(c) 2015 by Jeahong, Oh
+All rights are reserved.
+
+
+[Contact]
+E-mail: ojh6t3k@gmail.com
---
+++ Assets/SmartMaker/Scripts/CommOTG.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace SmartMaker
+{
+	[AddComponentMenu("SmartMaker/Communication/CommOTG")]
+	public class CommOTG : CommObject
+	{
+		void Awake()
+		{
+		}
+		
+		public override void Open()
+		{
+		}
+		
+		public override void Close()
+		{
+		}
+		
+		public override void Write(byte[] bytes)
+		{
+			try
+			{
+			}
+			catch(Exception)
+			{
+				if(OnErrorClosed != null)
+					OnErrorClosed(this, null);
+			}
+		}
+		
+		public override byte[] Read()
+		{
+			List<byte> bytes = new List<byte>();
+			
+			while(true)
+			{			
+				try
+				{
+				}
+				catch(TimeoutException)
+				{
+					break;
+				}
+				catch(Exception)
+				{
+					if(OnErrorClosed != null)
+						OnErrorClosed(this, null);
+					return null;
+				}
+			}
+			
+			if(bytes.Count == 0)
+				return null;
+			else
+				return bytes.ToArray();
+		}
+		
+		public override bool IsOpen
+		{
+			get
+			{
+				return false;
+			}
+		}
+	}
+}
